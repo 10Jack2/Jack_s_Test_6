b92fd62b7b0823c0da69063f0be18b3bbeecb8cc
Jack Scrivener <10jack222@gmail.com>
2023-05-13 18:31:43 +1200

    Updated Controller inputs for all inputs

--- src/ControllerInputs.cpp
+++ src/ControllerInputs.cpp
@@ -4,8 +4,58 @@
 
  class ContorllerInputsClass{
     public:
+
     static void ControllerButtonAPressed(void){
         Brain.Screen.printAt(10, 20, "A button pressed !!!" );
+        motor1.spin(vex::forward);
+    };
+    static void ControllerButtonBPressed(void){
+        Brain.Screen.printAt(10, 20, "B button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonXPressed(void){
+        Brain.Screen.printAt(10, 20, "X button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonYPressed(void){
+        Brain.Screen.printAt(10, 20, "Y button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+
+
+    static void ControllerButtonL1Pressed(void){
+        Brain.Screen.printAt(10, 20, "L1 button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonL2Pressed(void){
+        Brain.Screen.printAt(10, 20, "L2 button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonR1Pressed(void){
+        Brain.Screen.printAt(10, 20, "R1 button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonR2Pressed(void){
+        Brain.Screen.printAt(10, 20, "R2 button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+
+
+    static void ControllerButtonUpPressed(void){
+        Brain.Screen.printAt(10, 20, "Up button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonDownPressed(void){
+        Brain.Screen.printAt(10, 20, "Down button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonLeftPressed(void){
+        Brain.Screen.printAt(10, 20, "Left button pressed !!!" );
+        Brain.Screen.clearScreen();
+    };
+    static void ControllerButtonRightPressed(void){
+        Brain.Screen.printAt(10, 20, "Right button pressed !!!" );
+        Brain.Screen.clearScreen();
     };
     private:
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,16 @@
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
 #include "RobotConfig.h"
+
 #include "ControllerInputs.cpp"
+
 
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
 brain Brain;
 
-controller Controller1 = controller(primary);
+vex::controller Controller1 = vex::controller(vex::primary);
 
 // define your global instances of motors and other devices here
 motor motor1 = motor(PORT1, ratio18_1, false);
@@ -23,8 +25,35 @@ motor motor2 = motor(PORT2, ratio18_1, false);
 
 ContorllerInputsClass ContorllerInputsobject;
 
-int main() {
+//Setsup the callback events for each button on the contorller
+//these are the functions that will be called when each button is pressed
+//The functions are in a seperate object which is created from the the class above
+void CallBackSetup(){
+    printf("Call Back Setup function called...");
+    //callback functions for A,B,X and Y buttons
     Controller1.ButtonA.pressed(ContorllerInputsobject.ControllerButtonAPressed);
+    Controller1.ButtonB.pressed(ContorllerInputsobject.ControllerButtonBPressed);
+    Controller1.ButtonX.pressed(ContorllerInputsobject.ControllerButtonXPressed);
+    Controller1.ButtonY.pressed(ContorllerInputsobject.ControllerButtonYPressed);
+
+    //callback functions for the bumbpers on the front of the controller
+    Controller1.ButtonL1.pressed(ContorllerInputsobject.ControllerButtonL1Pressed);
+    Controller1.ButtonL2.pressed(ContorllerInputsobject.ControllerButtonL2Pressed);
+    Controller1.ButtonR1.pressed(ContorllerInputsobject.ControllerButtonR1Pressed);
+    Controller1.ButtonR2.pressed(ContorllerInputsobject.ControllerButtonR2Pressed);
+
+    //callback functions for the up,donw,left and right arrow buttons
+    Controller1.ButtonUp.pressed(ContorllerInputsobject.ControllerButtonUpPressed);
+    Controller1.ButtonDown.pressed(ContorllerInputsobject.ControllerButtonDownPressed);
+    Controller1.ButtonLeft.pressed(ContorllerInputsobject.ControllerButtonLeftPressed);
+    Controller1.ButtonRight.pressed(ContorllerInputsobject.ControllerButtonRightPressed);
+    printf("Call back setup function compleated");
+}
+
+
+int main() {
+    printf("Main loop started...");
+    CallBackSetup();
     
 
     motor1.setVelocity(20,percent);
